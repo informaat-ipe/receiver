7dde0e73be45f4c0616d05388eb612e3ebaeb2ce
garbrand <garbrand@gmail.com>
2015-10-30 11:46:53 +0100

    Unwrapped the wrapper

--- sources/handler.js
+++ sources/handler.js
@@ -20,39 +20,37 @@ function checkForDuplicate( message ) {
 
 
 // Github webhook message handler
-module.exports = function handlerFactory( options ) {
-	return function handler ( req, res ){
-		console.log('Received as POST to /new-module');
+module.exports = function handler ( req, res ){
+	console.log('Received as POST to /new-module');
 
-		// Check payload
-		if( ! req.body || Object.keys(req.body) === 0 ) {
-			var error = "Received an empty payload.";
+	// Check payload
+	if( ! req.body || Object.keys(req.body) === 0 ) {
+		var error = "Received an empty payload.";
 
-			console.error("ERROR: ", error);
+		console.error("ERROR: ", error);
 
-			res.status(500, { error: error });
-			res.end('ERROR');
-		}
-
-
-		// Handle payload -- turn it into posts to the Teamcity API
-		sender( parser(req.body) )
-		.then( function summarizeResults( results ) {
-			return results.map( summarize );
-		})
-		.then( function success (summaries) {
-			console.log("SUCCESS", summaries);
-
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.end('SUCCESS');
-		})
-		.catch( function failure (error) {
-			console.error( 'ERROR:', error );
-
-			res.status(500, {
-				error: error
-			});
-			res.end('ERROR');
-		});
+		res.status(500, { error: error });
+		res.end('ERROR');
 	}
+
+
+	// Handle payload -- turn it into posts to the Teamcity API
+	sender( parser(req.body) )
+	.then( function summarizeResults( results ) {
+		return results.map( summarize );
+	})
+	.then( function success (summaries) {
+		console.log("SUCCESS", summaries);
+
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end('SUCCESS');
+	})
+	.catch( function failure (error) {
+		console.error( 'ERROR:', error );
+
+		res.status(500, {
+			error: error
+		});
+		res.end('ERROR');
+	});
 }
